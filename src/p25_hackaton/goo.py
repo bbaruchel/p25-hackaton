d89b86454b8bf6c3be055c4dce3844ef4ab1b2a1
Léna Mazingue <lenamazingue@gmail.com>
2026-01-30 10:03:04 +0100

    class goo

---
+++ src/p25_hackaton/goo.py
@@ -0,0 +1,12 @@
+class goo():
+    def __init__(self, position_x: float, position_y: float, speed: float = 1.0, rayon:float= 0.01, masse: float= 0.4):
+        
+        self.center_x = position_x
+        self.center_y = position_y
+        self.speed = speed
+        self.rayon = rayon
+        self.masse = masse
+
+    
+
+    

81651288f0417a23aa372738bb5bda20db676953
rom1922 <romain.s.giunta@gmail.com>
2026-01-30 10:21:07 +0100

    init display class

--- src/p25_hackaton/display.py
+++ src/p25_hackaton/display.py
@@ -1,1 +1,23 @@
 import pygame 
+from platform import Platform
+form goo import Goo
+
+class Display:
+    def __init__(self, width : float, height :float, start : Platform, end : Platform) -> None:
+        self.width = width
+        self.height = height
+        self.start = start
+        self.end = end
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("World of Goos")
+        self.clock = pygame.time.Clock()
+        pygame.init()
+
+        pygame.draw.rect(self.screen, (0, 255, 0), (start.x, start.y, start.width, start.height))
+        pygame.draw.rect(self.screen, (255, 0, 0), (end.x, end.y, end.width, end.height))
+
+    def clear(self) -> None:
+        self.screen.fill((0, 0, 0)) 
+
+    def add_goo(self, goo : Goo) -> None:
+        pygame.draw.circle(self.screen, (0, 0, 255), (goo.center_x, goo.center_y), goo.rayon)
--- src/p25_hackaton/platform.py
+++ src/p25_hackaton/platform.py
@@ -1,11 +1,13 @@
 
 class Platform: 
-    def __init__(self, x, y, width, height, range = 10):
+    def __init__(self, x : float, y : float, width : float, height : float, start : bool, end : bool, range : float = 0.01) -> None:
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.range = range
+        self.start = start
+        self.end = end
 
     def in_reach(self, x, y):
         return self.x - self.range <= x <= self.x + self.width + self.range and self.y - self.range - self.height <= y <= self.y  + self.range 

b6e9aea208d57944c8c7e19a11682f8f26ccadf0
Léna Mazingue <lenamazingue@gmail.com>
2026-01-30 10:59:51 +0100

    G au lieu de g

--- src/p25_hackaton/goo.py
+++ src/p25_hackaton/goo.py
@@ -1,4 +1,4 @@
-class goo():
+class Goo():
     def __init__(self, position_x: float, position_y: float, speed: float = 1.0, rayon:float= 0.01, masse: float= 0.4):
         
         self.center_x = position_x

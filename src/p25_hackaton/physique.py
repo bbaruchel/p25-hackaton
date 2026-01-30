e22b9c907c35ef1422a2dea1ba224053084420e2
rom1922 <romain.s.giunta@gmail.com>
2026-01-30 15:15:57 +0100

    integration euler

--- src/p25_hackaton/physique.py
+++ src/p25_hackaton/physique.py
@@ -42,15 +42,23 @@ def forces(goos):
     return F
 
 
-def verlet_integration_bis(goos, delta_t):
-    A = np.array(forces(goos))/masse_goo
+""" def verlet_integration_bis(goos, delta_t):
+   A = np.array(forces(goos))/masse_goo
     #Mise à jour
-    for i in range(len(goos)): 
-        goos[i].x, goos[i].y = goos[i].x +goos[i].vx * delta_t + (1/2)*A[i][0]*(delta_t)**2, goos[i].y + goos[i].vy * delta_t + (1/2)*A[i][1]*(delta_t)**2
+   for i in range(len(goos)): 
+        goos[i].x, goos[i].y = goos[i].x +0.9*(goos[i].vx * delta_t + (1/2)*A[i][0]*(delta_t)**2), goos[i].y + 0.9*(goos[i].vy * delta_t + (1/2)*A[i][1]*(delta_t)**2)
     n_A = np.array(forces(goos))/masse_goo
     for i in range(len(goos)):
-        goos[i].vx ,goos[i].vy = goos[i].vx + delta_t*(A[i][0] + n_A[i][0])/2, goos[i].vy + delta_t*(A[i][1] + n_A[i][1])/2
-    
+        goos[i].vx ,goos[i].vy = goos[i].vx + 0.9*(delta_t*(A[i][0] + n_A[i][0])/2), goos[i].vy + 0.9*(delta_t*(A[i][1] + n_A[i][1])/2)
+     """
+
+def euler_integration(goos, delta_t):
+    A = np.array(forces(goos))/masse_goo
+    for i in range(len(goos)):
+        goos[i].vx ,goos[i].vy = goos[i].vx + delta_t*A[i][0], goos[i].vy + delta_t*A[i][1]
+        goos[i].x, goos[i].y = goos[i].x + goos[i].vx * delta_t, goos[i].y + goos[i].vy * delta_t
+
+
 def spring_update(goos) : 
   visite = [False]*len(goos)
   for g in goos :

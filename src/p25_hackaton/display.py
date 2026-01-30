164ecaca401a8440a4e74c1ec3925b91995c60c9
rom1922 <romain.s.giunta@gmail.com>
2026-01-30 10:38:28 +0100

    class spring

--- src/p25_hackaton/display.py
+++ src/p25_hackaton/display.py
@@ -1,6 +1,6 @@
 import pygame 
-from platform import Platform
-form goo import Goo
+from .platform import Platform
+from .goo import Goo
 
 class Display:
     def __init__(self, width : float, height :float, start : Platform, end : Platform) -> None:
@@ -12,12 +12,15 @@ class Display:
         pygame.display.set_caption("World of Goos")
         self.clock = pygame.time.Clock()
         pygame.init()
+        self._clear()
 
+        #draw start and end platforms
         pygame.draw.rect(self.screen, (0, 255, 0), (start.x, start.y, start.width, start.height))
         pygame.draw.rect(self.screen, (255, 0, 0), (end.x, end.y, end.width, end.height))
 
-    def clear(self) -> None:
+    def _clear(self) -> None:
         self.screen.fill((0, 0, 0)) 
 
-    def add_goo(self, goo : Goo) -> None:
+    def _add_goo(self, goo : Goo) -> None:
         pygame.draw.circle(self.screen, (0, 0, 255), (goo.center_x, goo.center_y), goo.rayon)
+
